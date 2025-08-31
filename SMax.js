2c888a12b6ba293b75f57592a3984b0b5e893a44
Muhammad Talha Khan <Talhakhan050203@gmail.com>
2025-08-31 17:16:14 +0500

    All PlayList

---
+++ SMax.js
@@ -0,0 +1,21 @@
+
+let marks = [90]
+
+let first = -Infinity;
+let second = -Infinity;
+let i;
+for(i = 0 ; i<marks.length;i++){
+     if(marks[i]> first){
+        second =first;
+        first =marks[i]
+     }
+     else if(marks[i]> second && marks[i]< first){
+        second = marks[i]
+     }
+}
+if(second === -Infinity){
+    console.log("NO Second Max Found", second)
+}
+else{
+    console.log("Second Max Found : ",second)
+}
